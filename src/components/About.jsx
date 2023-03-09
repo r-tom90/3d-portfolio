a50271eb481fc621c058703c1c36f4fe7c0cda1c
CryptoFallen <richardtom1203@protonmail.com>
2023-03-09 10:52:39 +0000

    About section completed. ServiceCard Component was created in the About Component. A higher order component was cerated (SectionWrapper) to allow the rest of the components to be wrapped in it with the framer motion package.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+// A package to allow for tilting of cards
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="w-full xs:w-[250px]">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="green-pink-gradient w-full rounded-[20px] p-[1px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] bg-tertiary py-5 px-12"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="h-16 w-16 object-contain"
+        />
+
+        <h3 className="text-center text-[20px] font-bold text-white">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 max-w-3xl text-[17px] leading-[30px] text-secondary"
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+// Utilizing HigherOrderComponents to wrap
+export default SectionWrapper(About, "about");

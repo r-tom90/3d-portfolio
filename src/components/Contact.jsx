62603c5c7bae27eeb72a90b85bb0926bb0121ad4
CryptoFallen <richardtom1203@protonmail.com>
2023-03-10 11:55:53 +0000

    Form Function complete, able to recieve email requests with emailjs. Base part of the project is now complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,15 +32,21 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
+    // emailjs service
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
+          // Name of who is sending the request
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          // Name of who is receiving the request
+          to_name: "Richard Tom",
+          // Email of who is sending the request
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          // Email of who is receiving the request
+          to_email: "richardtom1203@gmail.com",
+          // Message of who is sending the request
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
